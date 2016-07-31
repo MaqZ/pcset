94d499e78e11d11a5285809f3af2c316df1c6e7b
MaqZ <maqusser@gmail.com>
2016-07-31 18:55:59 +0200

    Added repo bash scripts

---
+++ from_repo.sh
@@ -0,0 +1,13 @@
+# HOME
+sudo cp ./home/.config/i3/config $HOME/.config/i3/config
+sudo cp -Rf ./home/.oh-my-zsh/ $HOME/.oh-my-zsh/
+sudo cp -Rf ./home/.vim/ $HOME/.vim/
+sudo cp ./home/ $HOME/
+
+# USR/BIN
+sudo cp ./usr/bin/ansiweather /usr/bin/ansiweather
+sudo cp ./usr/bin/timer /usr/bin/timer
+sudo cp ./usr/bin/wismysys /usr/bin/wismysys
+
+# ETC
+sudo cp ./etc/vimrc /etc/vimrc
---
+++ to_repo.sh
@@ -0,0 +1,13 @@
+# HOME
+sudo cp $HOME/.config/i3/config ./home/.config/i3/config
+sudo cp -Rf $HOME/.oh-my-zsh/ ./home/.oh-my-zsh/
+sudo cp -Rf $HOME/.vim/ ./home/.vim/
+sudo cp $HOME/ ./home/
+
+# USR/BIN
+sudo cp /usr/bin/ansiweather ./usr/bin/ansiweather
+sudo cp /usr/bin/timer ./usr/bin/timer
+sudo cp /usr/bin/wismysys ./usr/bin/wismysys
+
+# ETC
+sudo cp /etc/vimrc ./etc/vimrc
